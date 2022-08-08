37208f4e7d0fc11018f08a56315edbcc35a2308c
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-09 01:08:29 +0430

    add country setting to user

--- src/app/services/auth-header.js
+++ src/app/services/auth-header.js
@@ -1,13 +1,24 @@
-export default function authHeader() {
+/**
+ * 
+ * @returns {String} return the Bearer token for Authorization header
+ */
+ export default function authHeader() {
+  /**
+   * get the token from local storage
+   */
   const user = JSON.parse(localStorage.getItem("user"));
-
-  if (user && user.accessToken) {
+ /**
+  * if token is not null then return the token
+  */
+  if (user && user.access) {
     // For Spring Boot back-end
-    // return { Authorization: "Bearer " + user.accessToken };
-
-    // for Node.js Express back-end
-    return { "x-access-token": user.accessToken };
+    return  "Bearer " + user.access ;
+    /**
+     * else return null
+     */
   } else {
-    return {};
+    return {
+      
+    };
   }
 }
--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -1,32 +1,93 @@
 import axios from "axios";
-
+/**
+ * @description: This is the auth service class
+ * @class AuthService
+ *  
+ *
+ */
+/**
+ * define constat for base url
+ * @constant {string} API_URL Url for login api
+ * @constant {string} USER_PROFILE Url for user profile api
+ */
 const API_URL = "http://5.182.47.38:8001/auth/login/";
-
+const USER_PROFILE = "http://5.182.47.38:8001/user-data/";
 class AuthService {
+  /**
+   * 
+   * @param {string} username  user username
+   * @param {string} password  user password 
+   * @returns axios promise 
+   * @memberof AuthService
+   * @public
+   * @function login
+   * @description login user
+   * 
+   */
   login(username, password) {
     return axios
       .post(API_URL , { username, password })
       .then((response) => {
-        console.log(response);
+        /**
+         * if login success then save token to local storage
+         */
         if (response.data.access) {
-          localStorage.setItem("user", JSON.stringify(response.data));
+         const user={
+           refresh:response.data.refresh,
+            access:response.data.access,
+         }
+         /**
+          * get user profile from api
+          */
+         return axios.get(USER_PROFILE  ,  { headers: {Authorization:"Bearer "+user.access} }).then
+          (res=>{
+            /**
+             * if user profile is not null then save user profile to local storage
+             */
+            const country=res.data.country;
+            user.id=res.data.user.pk;
+            user.admin=res.data.user.is_superuser;
+            user.name=res.data.user.name;
+            user.username=res.data.user.username;
+            user.idnumber=res.data.user.idnumber;
+            user.phone=res.data.user.phone;
+            user.facility_admin=res.data.user.facadmin;
+            user.facility_id=res.data.user.facilityid;
+            user.reportadmin=res.data.user.reportadmin;
+            user.useradmin=res.data.user.useradmin;
+            user.created_at=res.data.user.created_at.split("T")[0];
+            user.updated_at=res.data.user.updated_at.split("T")[0];
+          localStorage.setItem("user", JSON.stringify(user));
+          localStorage.setItem("country", JSON.stringify(country));
+          return response.data;
+          })
         }
+        
 
-        return response.data;
+        
       });
   }
-
+  /**
+   * remove user from local storage
+   * @memberof AuthService
+   * @public
+   * @function logout
+   */
   logout() {
     localStorage.removeItem("user");
+    localStorage.removeItem("country");
   }
-
-  register(username, email, password) {
-    return axios.post(API_URL + "signup", {
-      username,
-      email,
-      password,
-    });
-  }
+/**
+ * if you done register api then you can use this function to register user
+ * uncomment the code and run the app
+ */
+  // register(username, email, password) {
+  //   return axios.post(API_URL + "signup", {
+  //     username,
+  //     email,
+  //     password,
+  //   });
+  // }
 }
 
 export default new AuthService();
