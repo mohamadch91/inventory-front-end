06c4cec391fbc0e30b75efa29c7e40c96e433901
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-12 13:58:19 +0430

    new build

---
+++ build/static/js/414.b71b11a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinventory=self.webpackChunkinventory||[]).push([[414],{7414:function(n,e,t){t.r(e);var r=t(184);e.default=function(){return(0,r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=414.b71b11a4.chunk.js.map
