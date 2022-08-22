2c8eeacc4100c75cef347e60890d72ad2dbdc736
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-22 06:15:56 +0430

    change API URL

--- src/app/services/user-list.service.js
+++ src/app/services/user-list.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://127.0.0.1:8000/auth/";
+const API_URL = "http://5.182.47.38:8001/auth/";
 
 class UserListService {
   getUsersByFacilityId(id) {
