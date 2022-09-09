19f108dfa815ffeeb8fe862944b80bce87e72f08
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-09 06:46:10 +0430

    new build

--- src/app/services/help.service.js
+++ src/app/services/help.service.js
@@ -1,7 +1,7 @@
 import ApiManager from "./axios-config";
 import authHeader from "./auth-header";
 
-const API_URL = "http://127.0.0.1:8000/help/";
+const API_URL = "http://5.182.47.38:8001/help/";
 
 class HelpService {
   getHelpContent(lang, page) {
