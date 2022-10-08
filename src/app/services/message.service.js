816277ed799c2af5fc9c7e6f0fcc1dfe7e24e97f
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-08 13:34:31 +0330

    for demo api 3

--- src/app/services/message.service.js
+++ src/app/services/message.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "https://demoapi2.invgap.org/message/";
+const API_URL = "https://demoapi3.invgap.org/message/";
 
 class MessageService {
   getReceivedMessages() {
