b22b529abe595d7a51cbbf64b7eb90ff520c0659
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-13 02:56:18 +0330

    build for dlb

--- src/app/services/language.service.js
+++ src/app/services/language.service.js
@@ -1,7 +1,7 @@
 import ApiManager from "./axios-config";
 import authHeader from "./auth-header";
 
-const API_URL = "https://demoapi1.invgap.org/languages/";
+const API_URL = "https://demoapi2.invgap.org/languages/";
 
 class HRService {
   getTranslations() {
