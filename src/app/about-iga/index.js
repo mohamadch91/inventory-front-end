8669a4864708dd8ea85f86ac0eb22017ff91e4f6
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-14 14:30:37 +0330

    add doc for about-iga

--- src/app/about-iga/index.js
+++ src/app/about-iga/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Help from "../components/Help";
-
+/**
+ * Component for the About IGA page.
+ * @returns {JSX.Element} The About IGA page.
+ * use the Help component to display the help text
+ */
 function AboutIGA() {
   return (
     <div>
--- src/app/actions/auth.js
+++ src/app/actions/auth.js
@@ -6,44 +6,59 @@ import {
   LOGOUT,
   SET_MESSAGE,
 } from "./types";
-
 import AuthService from "../services/auth.service";
+/*
+import Authentication servisece from services folder
+*/
+/**
+ * @param  {get username for register} username
+ * @param  {get email for register} email
+ * @param  {get password for register} password
+ * @returns {dispatch} dispatch
+ * if you register successfully, you will get a message
+ * if you register fail, you will get a message
+ * if you want to add register function please add it in the auth.service.js
+ * and add the action in the auth.js
+ * and add the reducer in the auth.js
+ * and add the type in the auth.js
+ * and uncomment the register function in the auth.service.js
+ * and uncomment the register function in the auth.js
+ */
+// export const register = (username, email, password) => (dispatch) => {
+//   return AuthService.register(username, email, password).then(
+//     (response) => {
+//       dispatch({
+//         type: REGISTER_SUCCESS,
+//       });
 
-export const register = (username, email, password) => (dispatch) => {
-  return AuthService.register(username, email, password).then(
-    (response) => {
-      dispatch({
-        type: REGISTER_SUCCESS,
-      });
+//       dispatch({
+//         type: SET_MESSAGE,
+//         payload: response.data.message,
+//       });
 
-      dispatch({
-        type: SET_MESSAGE,
-        payload: response.data.message,
-      });
+//       return Promise.resolve();
+//     },
+//     (error) => {
+//       const message =
+//         (error.response &&
+//           error.response.data &&
+//           error.response.data.message) ||
+//         error.message ||
+//         error.toString();
 
-      return Promise.resolve();
-    },
-    (error) => {
-      const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
+//       dispatch({
+//         type: REGISTER_FAIL,
+//       });
 
-      dispatch({
-        type: REGISTER_FAIL,
-      });
+//       dispatch({
+//         type: SET_MESSAGE,
+//         payload: message,
+//       });
 
-      dispatch({
-        type: SET_MESSAGE,
-        payload: message,
-      });
-
-      return Promise.reject();
-    }
-  );
-};
+//       return Promise.reject();
+//     }
+//   );
+// };
 
 export const login = (username, password) => (dispatch) => {
   return AuthService.login(username, password).then(
