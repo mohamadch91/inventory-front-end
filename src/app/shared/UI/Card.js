adb3751ecf59c6fdfc3fe5c52a7d4e4e4d901448
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-15 15:32:21 +0330

    feat: docs for shared/cards

--- src/app/shared/UI/Card.js
+++ src/app/shared/UI/Card.js
@@ -3,6 +3,7 @@ import classes from "./Card.module.css";
 /**
  *   return div card component with special styles
  * @param  {props} props
+ * uses in all card
  */
 const Card = (props) => {
   return <div className={classes.card}>{props.children}</div>;
