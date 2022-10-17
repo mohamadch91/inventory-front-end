44dd8da4b764d6a5355a003c3bc5a7c301df9e8d
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-17 17:43:56 +0330

    chnage message service facility list api

--- src/app/services/message.service.js
+++ src/app/services/message.service.js
@@ -1,31 +1,35 @@
 import axios from "axios";
+import ApiManager from "./axios-config";
 import authHeader from "./auth-header";
 
 const API_URL = "http://127.0.0.1:8000/message/";
 
 class MessageService {
   getReceivedMessages() {
-    return axios.get(API_URL, {
+    return ApiManager.get(API_URL, {
       headers: { Authorization: authHeader() },
     });
   }
   getSentMessages() {
-    return axios.get(API_URL + "?type=sender", {
+    return ApiManager.get(API_URL + "?type=sender", {
       headers: { Authorization: authHeader() },
     });
   }
   sendMessage(data) {
-    return axios.post(API_URL, data, {
+    return ApiManager.post(API_URL, data, {
       headers: { Authorization: authHeader() },
     });
   }
   putMessage(data) {
-    return axios.put(API_URL, data, {
+    return axiApiManageros.put(API_URL, data, {
       headers: { Authorization: authHeader() },
     });
   }
   getfacilities(){
-    
+    return ApiManager.get(API_URL+'helper', {
+      headers: { Authorization: authHeader() },
+    });
+
   }
 }
 
