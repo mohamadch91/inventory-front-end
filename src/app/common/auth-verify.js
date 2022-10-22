63b0833ebbf38496abddcaf7c95e4c1ab591de6b
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-22 14:40:04 +0330

    complete docs for common folder

--- src/app/common/auth-verify.js
+++ src/app/common/auth-verify.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { history } from '../helpers/history';
-
+/**
+ * @param  {JWTTOKEN} token
+ * 
+ */
 const parseJwt = (token) => {
   try {
     return JSON.parse(atob(token.split('.')[1]));
@@ -9,23 +12,30 @@ const parseJwt = (token) => {
   }
 };
 
+
+/**
+ * @param  {reducers state} props
+ * component for check if token is  invalid logout the user check token expire time 
+ * and now time if token is expired logout the user
+ */
 class AuthVerify extends Component {
   constructor(props) {
     super(props);
-
+    //check for history change
     history.listen(() => {
+      //GET THE TOKEN FROM THE LOCAL STORAGE
       const user = JSON.parse(localStorage.getItem("user"));
-
+      //if user logged in  
       if (user) {
         const decodedJwt = parseJwt(user.accessToken);
-
+        //if token is expired logout the user
         if (decodedJwt.exp * 1000 < Date.now()) {
           props.logOut();
         }
       }
     });
   }
-
+  //render nothing
   render() {
     return <div></div>;
   }
