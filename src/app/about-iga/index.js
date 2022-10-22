242de122b3175300549d837e4e32e5cde0b2773d
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-22 13:45:53 +0330

    edit aboutIGA page

--- src/app/about-iga/index.js
+++ src/app/about-iga/index.js
@@ -1,14 +1,18 @@
+import { Language } from "@mui/icons-material";
 import React from "react";
 import Help from "../components/Help";
+import i18n from "../../i18n";
+
 /**
  * Component for the About IGA page.
  * @returns {JSX.Element} The About IGA page.
  * use the Help component to display the help text
  */
 function AboutIGA() {
+  const language= i18n.language;
   return (
     <div>
-      <Help selectedLang="english" selectedPage="admin_guide" />
+      <Help selectedLang={language} selectedPage="admin_guide" />
     </div>
   );
 }
