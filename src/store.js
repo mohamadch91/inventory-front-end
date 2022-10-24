9962976b6bb5a630046c6e52279d941d12d67170
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-24 11:50:32 +0330

    docs for store.js

--- src/store.js
+++ src/store.js
@@ -3,6 +3,14 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./app/reducers";
 
+/**
+ * Create a redux store with the root reducer and middleware
+ * @type {Store}
+ * @property {Object} state - the state of the store
+ * @property {Function} dispatch - the dispatch function of the store
+ * @property {Function} subscribe - the subscribe function of the store
+ * 
+ */
 const middleware = [thunk];
 
 const store = createStore(
