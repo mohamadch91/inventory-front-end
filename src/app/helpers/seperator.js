32e8226edfe2c674fc26fc09a11ff22efb92c2c5
mohamadch91 <mohamadchoupan80@gmail.com>
2022-10-27 15:27:04 +0330

    feat: docs for helpers

--- src/app/helpers/seperator.js
+++ src/app/helpers/seperator.js
@@ -3,7 +3,11 @@ import i18n from "../../i18n";
 export const getLanguage = () => {
     return i18n.language;
     }
-
+/**
+ * function check float numbers floating point
+ * @returns String "," or "." 
+ * 
+ */
 export const seperator = () => {
     if(getLanguage() === "ar" || getLanguage() === "en"){
         return ".";
