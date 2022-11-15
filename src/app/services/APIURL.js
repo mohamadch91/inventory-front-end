fb8336f12822cb1dc4ecd1e679967ab4d1d624d1
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-15 08:30:14 +0330

    fix: change urls for ukr

--- src/app/services/APIURL.js
+++ src/app/services/APIURL.js
@@ -1,2 +1,2 @@
-const API_URL = " https://tlsapi.invgap.org";
+const API_URL = " https://ukrapi.invgap.org";
 export default API_URL;
--- src/app/services/language.service.js
+++ src/app/services/language.service.js
@@ -1,7 +1,7 @@
 import ApiManager from "./axios-config";
 import authHeader from "./auth-header";
 
-const API_URL = " https://tlsapi.invgap.org/languages/";
+const API_URL = " https://ukrapi.invgap.org/languages/";
 
 class HRService {
   getTranslations() {
