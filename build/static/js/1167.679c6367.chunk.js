725db755762fd97114b6f6f7d1f662a3c9333412
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-26 18:22:44 +0330

    build for PHL

--- build/static/js/1167.679c6367.chunk.js
+++ build/static/js/1167.679c6367.chunk.js
@@ -8,7 +8,7 @@
         l = i(74569),
         a = i.n(l),
         r = i(54318),
-        t = "https://tlsapi.invgap.org/hr/",
+        t = "https://phlapi.invgap.org/hr/",
         d = (function () {
           function e() {
             (0, s.Z)(this, e);
@@ -117,7 +117,7 @@
         p = i(43144),
         v = i(39877),
         b = i(54318),
-        g = "https://tlsapi.invgap.org/auth/",
+        g = "https://phlapi.invgap.org/auth/",
         k = new ((function () {
           function e() {
             (0, f.Z)(this, e);
