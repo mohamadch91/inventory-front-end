7ecb63e6f40692f8e62b2328bc2d25a47b41c9e6
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-27 05:58:16 +0330

    new build for JOR

--- build/static/js/6675.631d56d5.chunk.js
+++ build/static/js/6675.631d56d5.chunk.js
@@ -1791,7 +1791,7 @@
         s = l(43144),
         c = l(39877),
         i = l(54318),
-        a = (l(74569), "https://api.invgap.org/facilities/"),
+        a = (l(74569), "https://jorapi.invgap.org/facilities/"),
         r = (function () {
           function e() {
             (0, t.Z)(this, e);
@@ -1855,7 +1855,7 @@
                 key: "importFacilities",
                 value: function (e) {
                   return c.Z.post(
-                    "https://api.invgap.org/facilities/import",
+                    "https://jorapi.invgap.org/facilities/import",
                     e,
                     { headers: { Authorization: (0, i.Z)() } }
                   );
@@ -1888,7 +1888,7 @@
         s = l(43144),
         c = l(54318),
         i = l(39877),
-        a = "https://api.invgap.org/reports/",
+        a = "https://jorapi.invgap.org/reports/",
         r = (function () {
           function e() {
             (0, t.Z)(this, e);
