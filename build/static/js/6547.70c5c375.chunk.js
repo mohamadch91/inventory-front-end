72b524639d78e9c264f3d46ac36bcf9a68ddb310
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-30 04:02:31 +0330

    feat: new build for PHL subdomain

--- build/static/js/6547.70c5c375.chunk.js
+++ build/static/js/6547.70c5c375.chunk.js
@@ -431,7 +431,7 @@
         i = s(74569),
         t = s.n(i),
         l = s(54318),
-        a = "https://jorapi.invgap.org/hr/",
+        a = "https://phlapi.invgap.org/hr/",
         d = (function () {
           function e() {
             (0, c.Z)(this, e);
--- build/static/js/8019.b07cbf79.chunk.js
+++ build/static/js/8019.b07cbf79.chunk.js
@@ -7,7 +7,7 @@
         t = i(43144),
         s = i(39877),
         c = i(54318),
-        l = "https://jorapi.invgap.org/maintanance/",
+        l = "https://phlapi.invgap.org/maintanance/",
         r = (function () {
           function e() {
             (0, a.Z)(this, e);
