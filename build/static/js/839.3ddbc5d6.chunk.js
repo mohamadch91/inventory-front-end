668bc7bf990bc9ef9d6bec0282a4c4a0d2f19639
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-14 12:34:18 +0330

    refactor: change API for phl

--- build/static/js/839.3ddbc5d6.chunk.js
+++ build/static/js/839.3ddbc5d6.chunk.js
@@ -1656,7 +1656,7 @@
         t = l(43144),
         c = l(39877),
         i = l(54318),
-        a = (l(74569), "https://tlsapi.invgap.org/facilities/"),
+        a = (l(74569), "https://phlapi.invgap.org/facilities/"),
         r = (function () {
           function e() {
             (0, s.Z)(this, e);
@@ -1720,7 +1720,7 @@
                 key: "importFacilities",
                 value: function (e) {
                   return c.Z.post(
-                    "https://tlsapi.invgap.org/facilities/import",
+                    "https://phlapi.invgap.org/facilities/import",
                     e,
                     { headers: { Authorization: (0, i.Z)() } }
                   );
@@ -1753,7 +1753,7 @@
         t = l(43144),
         c = l(54318),
         i = l(39877),
-        a = "https://tlsapi.invgap.org/reports/",
+        a = "https://phlapi.invgap.org/reports/",
         r = (function () {
           function e() {
             (0, s.Z)(this, e);
