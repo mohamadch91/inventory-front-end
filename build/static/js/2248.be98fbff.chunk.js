9d223b0546ca83dc78b65b6bf1a25bca89cb62e9
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-01 15:19:11 +0330

    refactor: apply changes for invgap

--- build/static/js/2248.be98fbff.chunk.js
+++ build/static/js/2248.be98fbff.chunk.js
@@ -7,7 +7,7 @@
         l = n(43144),
         a = n(39877),
         t = n(54318),
-        i = "https://jorapi.invgap.org/maintanance/",
+        i = "https://api.invgap.org/maintanance/",
         r = (function () {
           function e() {
             (0, c.Z)(this, e);
--- build/static/js/5676.f28cccf5.chunk.js
+++ build/static/js/5676.f28cccf5.chunk.js
@@ -743,7 +743,7 @@
         Y = a(43144),
         ee = a(39877),
         te = a(54318),
-        ae = "https://jorapi.invgap.org/languages/",
+        ae = "https://api.invgap.org/languages/",
         ne = new ((function () {
           function e() {
             (0, K.Z)(this, e);
